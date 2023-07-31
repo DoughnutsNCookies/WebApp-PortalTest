fa21a9022f3cdfc1aa7ba186766f7cc39166e218
Sean Chuah <chuahtseyung2002@gmail.com>
2023-07-31 18:32:45 +0800

    Added colors yahoo

--- WebApp-PortalTest/backend/src/index.ts
+++ WebApp-PortalTest/backend/src/index.ts
@@ -6,6 +6,9 @@ const host = app.get('host')
 
 process.on('unhandledRejection', (reason) => logger.error('Unhandled Rejection %O', reason))
 
+const LCYAN = '\x1b[96m'
+const RESET = '\x1b[0m'
+
 app.listen(port).then(() => {
-  logger.info(`Feathers app listening on http://${host}:${port}`)
+  logger.info(LCYAN + `Feathers app listening on http://${host}:${port}` + RESET)
 })
