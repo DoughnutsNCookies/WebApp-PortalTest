76febdced56be6e92ff7b6f3861722ad81d390e4
ijon <itan@student.42kl.edu.my>
2023-08-01 17:15:55 +0800

    now can logout

--- WebApp-PortalTest/frontend/src/pages/ProfilePage.vue
+++ WebApp-PortalTest/frontend/src/pages/ProfilePage.vue
@@ -1,12 +1,22 @@
 <template>
   <q-page padding class="q-gutter-md">
-    <q-btn label="Logout"/>
+    <q-btn label="Logout" @click="logout"/>
   </q-page>
 </template>
 
 <script>
 import { defineComponent } from 'vue'
+import { useRouter } from 'vue-router'
 export default defineComponent({
-  name: 'ProfilePage'
+  name: 'ProfilePage',
+  setup() {
+    const router = useRouter();
+    
+    const logout = () => {
+      localStorage.removeItem('accessToken');
+      router.push('/login');
+    }
+    return { logout }
+  }
 })
 </script>
